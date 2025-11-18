5b05d2d64586302e5944055fb2c660a85d141afd
cichy-michal <michau_cichau@interia.eu>
2025-11-18 14:01:39 +0100

    Dane widoczne w InfluxDB i Grafana

--- warehouse.py
+++ warehouse.py
@@ -2,12 +2,11 @@
 
 import logging
 import time
-import datetime
 
 from bme280 import BME280
 from smbus2 import SMBus
 
-from influxdb_client import InfluxDBClient, Point, WritePrecision
+import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 logging.basicConfig(
@@ -27,31 +26,27 @@ org = "pwr"
 token = "twoj_token"
 url = "http://localhost:8086"
 
-client = InfluxDBClient(url=url, token=token, org=org)
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
+try:
+    while True:
+        temperature = bme280.get_temperature()
+        pressure = bme280.get_pressure()
+        humidity = bme280.get_humidity()
 
-while True:
-    temperature = bme280.get_temperature()
-    pressure = bme280.get_pressure()
-    humidity = bme280.get_humidity()
+        logging.info(f"""
+    Temperature: {temperature:05.2f} °C
+    Pressure: {pressure:05.2f} hPa
+    Relative humidity: {humidity:05.2f} %
+    """)
 
-    logging.info(f"""
-Temperature: {temperature:05.2f} °C
-Pressure: {pressure:05.2f} hPa
-Relative humidity: {humidity:05.2f} %
-""")
-
-    point = (
-        Point("weather")
-        .field("temperature", temperature)
-        .field("pressure", pressure)
-        .field("humidity", humidity)
-        .time(datetime.datetime.utcnow(), WritePrecision.NS)
-    )
-    write_api.write(bucket=bucket, org=org, record=point)
-
-    time.sleep(1)
+        point = influxdb_client.Point("weather").field("temperature", temperature).field("pressure", pressure).field("humidity", humidity)
+        write_api.write(bucket=bucket, org=org, record=point)
+    
+        time.sleep(1)
+finally:
+    client.close()
